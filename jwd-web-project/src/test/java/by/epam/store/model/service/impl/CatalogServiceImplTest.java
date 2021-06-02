884dc66bd4c74c421f052b766caadfab9a0a40b7
Mariya Kuksar <mariya.kuksar@gmail.com>
2021-06-02 23:40:46 +0300

    Update project

--- jwd-web-project/src/test/java/by/epam/store/model/service/impl/CatalogServiceImplTest.java
+++ jwd-web-project/src/test/java/by/epam/store/model/service/impl/CatalogServiceImplTest.java
@@ -1,10 +1,12 @@
 package by.epam.store.model.service.impl;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -15,6 +17,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import by.epam.store.entity.Product;
 import by.epam.store.entity.ProductCategory;
 import by.epam.store.model.dao.DaoException;
 import by.epam.store.model.dao.ProductCategoryDao;
@@ -32,17 +35,25 @@ public class CatalogServiceImplTest {
 	private SupplyDao supplyDao;
 	private AutoCloseable autoCloseable;
 	private Map<String, String> productInfo;
+	private Product product;
 
 	@InjectMocks
 	CatalogServiceImpl catalogService;
 
 	@BeforeClass
 	public void setUp() {
+		autoCloseable = MockitoAnnotations.openMocks(this);
 		productInfo = new HashMap<>();
 		productInfo.put("productId", "1");
-		productInfo.put("price", "100");
-		productInfo.put("productName", "Desk");
-		autoCloseable = MockitoAnnotations.openMocks(this);
+		productInfo.put("price", "320");
+		productInfo.put("productName", "Desk TRIO");
+		product = new Product();
+		product.setProductId(1L);
+		product.setCategoryId(1L);
+		product.setProductName("Desk TRIO");
+		product.setImageName("desk.jpg");
+		product.setPrice(new BigDecimal(320));
+		product.setQuantity(10);
 	}
 
 	@AfterClass
@@ -80,5 +91,40 @@ public class CatalogServiceImplTest {
 		Mockito.when(productCategoryDao.findAll()).thenReturn(expected);
 		List<ProductCategory> actual = catalogService.takeAllProductCategories();
 		Assert.assertEquals(actual, expected);
+	}
+
+	@Test(expectedExceptions = ServiceException.class)
+	public void takeAllProductCategoriesTest02() throws DaoException, ServiceException {
+		Mockito.when(productCategoryDao.findAll()).thenThrow(DaoException.class);
+		catalogService.takeAllProductCategories();
+	}
+
+	@Test
+	public void takeProductByIdTest01() throws DaoException, ServiceException {
+		Optional<Product> expected = Optional.of(product);
+		Mockito.when(productDao.findProductById(Mockito.anyString())).thenReturn(expected);
+		Optional<Product> actual = catalogService.takeProductById("1");
+		Assert.assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void takeProductByIdTest02() throws DaoException, ServiceException {
+		Optional<Product> expected = Optional.empty();
+		Mockito.when(productDao.findProductById(Mockito.anyString())).thenReturn(expected);
+		Optional<Product> actual = catalogService.takeProductById("123568");
+		Assert.assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void takeProductByIdTest03() throws DaoException, ServiceException {
+		Optional<Product> expected = Optional.empty();
+		Optional<Product> actual = catalogService.takeProductById("f");
+		Assert.assertEquals(actual, expected);
+	}
+	
+	@Test (expectedExceptions = ServiceException.class)
+	public void takeProductByIdTest04() throws DaoException, ServiceException {
+		Mockito.when(productDao.findProductById(Mockito.anyString())).thenThrow(DaoException.class);
+		catalogService.takeProductById("1");
 	}
 }
